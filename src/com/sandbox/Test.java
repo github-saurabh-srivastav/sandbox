50d4f3daf938982d7c13b6dc7271978c4e33bca4
toshiba <toshiba@192.168.0.5>
2020-12-30 10:23:26 +0530

    first commit

---
+++ src/com/sandbox/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sandbox;
+
+/**
+ * @author toshiba
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
